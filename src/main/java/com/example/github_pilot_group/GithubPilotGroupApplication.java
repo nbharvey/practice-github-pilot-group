91dadfe83b2b75c68cb0aad248dd71e130f74d5d
dholmes5247 <dholmes5247@hotmail.com>
2025-07-08 16:49:05 -0500

    Darren Commit 2 With Program that runs error free currently

--- src/main/java/com/example/github_pilot_group/GithubPilotGroupApplication.java
+++ src/main/java/com/example/github_pilot_group/GithubPilotGroupApplication.java
@@ -1,9 +1,9 @@
 package com.example.github_pilot_group;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
+
 
 @SpringBootApplication
 public class GithubPilotGroupApplication {
